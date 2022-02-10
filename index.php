682cc1b7e6e89c5960343464719b40755ab0ca09
ImPaulinTech <impaulintech@gmail.com>
2022-02-11 00:19:18 +0800

    X

--- index.php
+++ index.php
@@ -2,17 +2,13 @@
 <html lang="en">
 
 <head>
-    <meta property="og:site_name" content="IMPAULINTECH">
-    <meta property="og:title" content="IMPAULINTECH">
-    <meta property="og:image" content="Your Site Image">
-    <meta property="og:description" content="I tell computers what to do, sometimes they listen">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IMPAULINTECH</title>
     <style>
         * {
-            margin: 0;
             padding: 0;
+            margin: 0;
         }
 
         body {
@@ -22,7 +18,7 @@
 </head>
 
 <body>
-    <iframe src="https://impaulintech.github.io/profile/" frameborder="0" style="width: 100%;height: 100%"></iframe>
+    <iframe src="https://impaulintech.github.io/profile/" frameborder="0" width="100%" height="100%"></iframe>
 </body>
 
 </html>
