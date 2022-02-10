208b637b9847633b99e037842af4f1ddb96bbc63
ImPaulinTech <impaulintech@gmail.com>
2022-02-11 00:12:19 +0800

    X

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta property="og:site_name" content="IMPAULINTECH">
+    <meta property="og:title" content="IMPAULINTECH">
+    <meta property="og:image" content="Your Site Image">
+    <meta property="og:description" content="I tell computers what to do, sometimes they listen">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMPAULINTECH</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            height: 100vh;
+        }
+    </style>
+</head>
+
+<body>
+    <iframe src="https://impaulintech.github.io/profile/" frameborder="0" style="width: 100%;height: 100%"></iframe>
+</body>
+
+</html>
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-//Server & Database connection
-let port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const DATABASE_URL = "mongodb+srv://admin:admin@csp2.tpt6x.mongodb.net/database?retryWrites=true&w=majority";
 
 //Middlewares
@@ -13,22 +12,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-//Connection to database
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-
-//Database status
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, `Error Connecting to Database.`));
-db.once('open', () => { console.log(`Connected to Database!`) })
-
-//temporary index page
-app.use("/", require('./routes/index'));
-
 //userRoutes module
 app.use("/users", require('./routes/userRoutes'));
 
 //productRoutes module
 app.use("/products", require('./routes/productRoutes'))
 
+//Connection to database
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+//Database status
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, `Error connecting to database`));
+db.once('open', () => { console.log(`Connected to database`) })
+
 //Server status
-app.listen(port, () => { console.log(`Server is running at PORT:${port}`) })
+app.listen(PORT, () => { console.log(`server is running at ${PORT}`) })
