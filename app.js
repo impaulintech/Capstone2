0cb41bc71e6bc609b61da1ea4c4c411fd5fb8878
ImPaulinTech <impaulintech@gmail.com>
2022-02-10 23:20:47 +0800

    x

--- app.js
+++ app.js
@@ -5,22 +5,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 //Server & Database connection
-let port = process.env.PORT || 5000;
+let port = 5000;
 const DATABASE_URL = "mongodb+srv://admin:admin@csp2.tpt6x.mongodb.net/database?retryWrites=true&w=majority";
 
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-//Temporary Index Page
-// app.use("/", require('./routes/index'));
-
-//userRoutes module
-app.use("/users", require('./routes/userRoutes'));
-
-//productRoutes module
-app.use("/products", require('./routes/productRoutes'))
 
 //Connection to database
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -30,5 +21,11 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, `Error Connecting to Database.`));
 db.once('open', () => { console.log(`Connected to Database!`) })
 
+//userRoutes module
+app.use("/users", require('./routes/userRoutes'));
+
+//productRoutes module
+app.use("/products", require('./routes/productRoutes'))
+
 //Server status
-app.listen(port, () => { console.log(`Server is running at PORT:${port}`) })
+app.listen(process.env.PORT || port, () => { console.log(`Server is running at PORT:${port}`) })
